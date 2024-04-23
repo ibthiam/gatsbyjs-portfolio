e8d4536dc1f62f4459ef6eb0ec13199f59858c45
Ibrahima E. THIAM <thiam.ibrahim@hotmail.com>
2024-04-23 09:27:15 +0100

    Update bookmark.js

--- src/components/icons/bookmark.js
+++ src/components/icons/bookmark.js
@@ -6,7 +6,7 @@ const IconBookmark = () => (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    strokeWidth="1"
+    strokeWidth="2"
     strokeLinecap="round"
     strokeLinejoin="round"
     className="feather feather-bookmark">
